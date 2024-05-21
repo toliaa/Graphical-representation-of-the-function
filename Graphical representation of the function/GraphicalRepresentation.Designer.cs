6ee3385be92ad1c4336afeee093c97502f3bbf3e
toliadriapak <toliadriapak@gmail.com>
2024-05-21 12:35:50 +0300

    Add project files.

---
+++ Graphical representation of the function/GraphicalRepresentation.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ParametricGraph
+{
+    partial class GraphicalRepresentation
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GraphicalRepresentation));
+            this.rTextBox = new System.Windows.Forms.TextBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.graphPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rTextBox
+            // 
+            this.rTextBox.Location = new System.Drawing.Point(428, 19);
+            this.rTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rTextBox.Name = "rTextBox";
+            this.rTextBox.Size = new System.Drawing.Size(132, 22);
+            this.rTextBox.TabIndex = 0;
+            this.rTextBox.Text = "1";
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(586, 16);
+            this.drawButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(100, 28);
+            this.drawButton.TabIndex = 1;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // graphPanel
+            // 
+            this.graphPanel.Location = new System.Drawing.Point(16, 49);
+            this.graphPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.graphPanel.Name = "graphPanel";
+            this.graphPanel.Size = new System.Drawing.Size(1090, 567);
+            this.graphPanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x = R * cos³(t)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "y = R * sin³(t)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 19);
+            this.label3.MaximumSize = new System.Drawing.Size(30, 30);
+            this.label3.MinimumSize = new System.Drawing.Size(15, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "{";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Formula";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 620);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "© 2024 Tolia Driapak";
+            // 
+            // GraphicalRepresentation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 641);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.graphPanel);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.rTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "GraphicalRepresentation";
+            this.Text = "Parametric Graph";
+            this.Load += new System.EventHandler(this.GraphicalRepresentation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox rTextBox;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Panel graphPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Graphical representation of the function/GraphicalRepresentation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ParametricGraph
+{
+    public partial class GraphicalRepresentation : Form
+    {
+        public GraphicalRepresentation()
+        {
+            InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            double R;
+            if (!double.TryParse(rTextBox.Text, out R))
+            {
+                MessageBox.Show("Please enter a valid numeric value for R.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DrawGraph(R);
+        }
+
+        private void DrawGraph(double R)
+        {
+            Bitmap bmp = new Bitmap(graphPanel.Width, graphPanel.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            // Clear the panel
+            g.Clear(Color.White);
+
+            // Draw coordinate system
+            Pen axisPen = new Pen(Color.Black, 2);
+            g.DrawLine(axisPen, graphPanel.Width / 2, 0, graphPanel.Width / 2, graphPanel.Height); // Y-axis
+            g.DrawLine(axisPen, 0, graphPanel.Height / 2, graphPanel.Width, graphPanel.Height / 2); // X-axis
+
+            // Draw labels
+            Font font = new Font("Arial", 10);
+            Brush brush = Brushes.Black;
+            g.DrawString("X", font, brush, graphPanel.Width - 20, graphPanel.Height / 2 + 5);
+            g.DrawString("Y", font, brush, graphPanel.Width / 2 + 5, 5);
+
+            // Calculate and draw the curve
+            Pen curvePen = new Pen(Color.Blue, 2);
+            double tStep = 0.01;
+            PointF? prevPoint = null;
+
+            for (double t = 0; t <= 2 * Math.PI; t += tStep)
+            {
+                double x = R * Math.Pow(Math.Cos(t), 3);
+                double y = R * Math.Pow(Math.Sin(t), 3);
+
+                // Transform the point to fit the panel
+                float drawX = (float)(graphPanel.Width / 2 + x * 50);
+                float drawY = (float)(graphPanel.Height / 2 - y * 50);
+
+                PointF currentPoint = new PointF(drawX, drawY);
+
+                if (prevPoint != null)
+                {
+                    g.DrawLine(curvePen, prevPoint.Value, currentPoint);
+                }
+
+                prevPoint = currentPoint;
+            }
+
+            // Display the graph
+            graphPanel.BackgroundImage = bmp;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GraphicalRepresentation_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
